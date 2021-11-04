b2b17bd6f5904c809c1527fd3046a530b464f29b
hy2850 <hy2850@naver.com>
2021-11-04 11:38:46 +0900

    [Added] PMX crossover for path Representation - Escapes early convergence, but still not very good - Reference Abid Hussain, Yousaf Shad Muhammad, M. Nauman Sajid, Ijaz Hussain, Alaa Mohamd Shoukry, Showkat Gani, "Genetic Algorithm for Traveling Salesman Problem with Modified Cycle Crossover Operator", Computational Intelligence and Neuroscience, vol. 2017, Article ID 7430125, 7 pages, 2017. https://doi.org/10.1155/2017/7430125

--- population.py
+++ population.py
@@ -4,10 +4,8 @@ import random
 
 class Population:
 	def __init__(self):
-		self.parentPop = []
-		for _ in range(config.POP_SIZE):
-			indiv = Individual()
-			self.parentPop.append(indiv)
+		self.parentPop = [Individual() for _ in range(config.POP_SIZE)]
+		self.matingPool = []
 		self.childPop = []
 		self.fitsum = 0
 
@@ -19,20 +17,27 @@ class Population:
 			self.fitsum += indiv.fitness
 
 	def selectParentTournament(self, K):
-		self.childPop.clear()
+		self.matingPool.clear()
 		for i in range(config.POP_SIZE):
 			tmp = []
 			for j in range(K):
 				tmp.append(self.parentPop[random.randint(0, config.POP_SIZE-1)])
 			bestAmongK = min(tmp, key=lambda indiv: indiv.fitness)
-			self.childPop.append(bestAmongK)
+			self.matingPool.append(bestAmongK)
 
 	def produceOffspring(self):
-		for indiv in self.childPop:
-			indiv.crossover()
+		self.childPop.clear()
+		for i in range(config.POP_SIZE//2):
+			parent1 = random.choice(self.parentPop)
+			parent2 = random.choice(self.parentPop)
+			off1, off2 = parent1.crossover_pmx(parent2)
 
-		for indiv in self.childPop:
-			indiv.mutation()
+			off1.mutation()
+			off2.mutation()
+
+			self.childPop.append(off1)
+			self.childPop.append(off2)
+
 
 	# Gradual replacement
 	def generationSelection(self, replaceCnt):
@@ -44,8 +49,9 @@ class Population:
 			self.parentPop[idx] = self.childPop[idx]
 
 	def printPop(self):
+		self.parentPop.sort(key=lambda indiv: indiv.fitness)
+
 		fitArr = []
 		for child in self.parentPop:
 			fitArr.append(child.fitness)
-		fitArr.sort()
 		print(fitArr)
